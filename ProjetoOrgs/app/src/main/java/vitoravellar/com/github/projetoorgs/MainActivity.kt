b5bfe59b01b6c518ac07fdb3e81af46693089a55
Vitor Avellar <vitor07sa@gmail.com>
2024-03-07 22:17:51 -0300

    Aula1

---
+++ ProjetoOrgs/app/src/main/java/vitoravellar/com/github/projetoorgs/MainActivity.kt
@@ -0,0 +1,21 @@
+package vitoravellar.com.github.projetoorgs
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import vitoravellar.com.github.projetoorgs.ui.theme.ProjetoOrgsTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+    }
+}
+
